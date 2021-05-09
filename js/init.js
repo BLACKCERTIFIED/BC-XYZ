5bede314b9b66b5f42a1ae26536efccf344574df
CERTIFIED Git <83636752+certified-git@users.noreply.github.com>
2021-05-09 05:45:30 +0300

    JS

---
+++ js/init.js
@@ -0,0 +1,34 @@
+
+(function () {
+	const second = 1000,
+		  minute = second * 60,
+		  hour = minute * 60,
+		  day = hour * 24;
+  
+	let launch = "Jan 30, 2022 00:00:00",
+		countDown = new Date(launch).getTime(),
+		x = setInterval(function() {    
+  
+		  let now = new Date().getTime(),
+			  distance = countDown - now;
+  
+		  document.getElementById("days").innerText = Math.floor(distance / (day)),
+			document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+			document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+			document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+		  //do something later when date is reached
+		  if (distance < 0) {
+			let headline = document.getElementById("headline"),
+				countdown = document.getElementById("countdown"),
+				content = document.getElementById("content");
+  
+			headline.innerText = "It's lauch day!";
+			countdown.style.display = "none";
+			content.style.display = "block";
+  
+			clearInterval(x);
+		  }
+		  //seconds
+		}, 0)
+	}());
